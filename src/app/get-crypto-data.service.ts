00ed9c5b8e46dfba28ff58464586bae4a659f74a
Bhanu Pratap Loshali <bhanuloshali11@gmail.com>
2018-02-05 23:51:27 +0530

    Update get-crypto-data.service.ts

--- src/app/get-crypto-data.service.ts
+++ src/app/get-crypto-data.service.ts
@@ -6,6 +6,7 @@ import { Observable } from 'rxjs/Observable';
 @Injectable()
 export class GetCryptoDataService{
   constructor(private http:Http  ) { }
+  //This function will fetch data from the given API and returns a Promise, which will be resolved by calling function 
   callApiForCryptoData(){
       return this.http.get("https://api.coinmarketcap.com/v1/ticker/?limit=10").map((res:Response)=>{return res.json()}).toPromise();
   }
